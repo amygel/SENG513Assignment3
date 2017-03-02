a921db1bf10f34496a59afbaeafc10fd88535321
Amy Gelowitz <amygelowitz@Jamess-MacBook-Pro.local>
2017-03-02 15:06:13 -0700

    Adding users

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// https://web.archive.org/web/20161018172034/http://socket.io/get-started/chat/
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+// http://psitsmike.com/2011/09/node-js-and-socket-io-chat-tutorial/
+// usernames which are currently connected to the chat
+var usernames = {};
 
 http.listen( port, function () {
     console.log('listening on port', port);
@@ -10,12 +15,24 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
-// listen to 'chat' messages
+// http://psitsmike.com/2011/09/node-js-and-socket-io-chat-tutorial/
 io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	    io.emit('chat', msg);
+    // listen to chat messages
+    socket.on('sendchat', function(msg){
+	    io.emit('updatechat', socket.username, msg);
     });
+    // listen to added users
+    socket.on('adduser', function(username){
+        socket.username = username;
+        usernames[username] = username;
+        socket.emit('updatechat', 'SERVER', 'you have connected');
+        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        io.sockets.emit('updateusers', usernames);
+    });
+    // listen for user disconnect
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
     });
 });
--- public/client.js
+++ public/client.js
@@ -2,11 +2,30 @@
 $(function() {
     var socket = io();
     $('form').submit(function(){
-	    socket.emit('chat', $('#m').val());
+	    socket.emit('sendchat', $('#m').val());
 	    $('#m').val('');
 	    return false;
     });
-    socket.on('chat', function(msg){
-	    $('#messages').append($('<li>').text(msg));
+    // on connection to server, add user to server
+    socket.on('connect', function(){
+        let num;
+        if ($('#users').count) {
+            num = $('#users').count + 1
+        } else {
+            num = 1
+        }
+
+        socket.emit('adduser', 'User' + num);
+    });
+    // update chat with new messages
+    socket.on('updatechat', function(username, msg){
+        $('#messages').append($('<b>'+username + ':</b> ' + msg + '<br>'));
+    });
+    // update user list
+    socket.on('updateusers', function(data) {
+        $('#users').empty();
+        $.each(data, function(key, value) {
+            $('#users').append('<div>' + key + '</div>');
+        });
     });
 });
